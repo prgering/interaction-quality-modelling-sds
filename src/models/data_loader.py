cdd710fc5019d8da528e6e724f0af3164f886d75
Paul Gering <prgering1@sheffield.ac.uk>
2026-09-08 13:27:39 +0100

    cleaned lstm training code

--- src/models/data_loader.py
+++ src/models/data_loader.py
@@ -20,7 +20,7 @@ def filter_embed_columns(df, args):
     columns_to_drop = []
 
     for col in df.columns:
-        if 'emb' not in col.lower() and 'dim' not in col.lower():
+        if 'non_pca' in col.lower():
             continue  # Skip non-embedding columns
         if text_tag not in col.lower() and speech_tag not in col.lower():
             columns_to_drop.append(col)
