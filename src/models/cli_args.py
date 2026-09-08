15b40112137cab0d41cbe4ca42153b066cfdab3c
Paul Gering <prgering1@sheffield.ac.uk>
2026-09-08 12:05:19 +0100

    cleaned lstm training code

--- src/models/cli_args.py
+++ src/models/cli_args.py
@@ -4,23 +4,23 @@ def add_feature_set_args(parser: argparse.ArgumentParser):
     """
     Adds common command-line arguments to a given parser object.
     """
-    parser.add_argument("dataset-type",
+    parser.add_argument("dataset_type",
                         choices=["system", "speech"],
                         default="system",
                         help="Specify the feature set to use."
     )
 
-    parser.add_argument("--pretrained-text-model", type=str, default="sbert")
-    parser.add_argument("--pretrained-speech-model",type=str,default="wav2vec2")
+    parser.add_argument("--pretrained_text_model", type=str, default="sbert")
+    parser.add_argument("--pretrained_speech_model",type=str,default="wav2vec2")
 
 def add_pca_args(parser: argparse.ArgumentParser):
     """
     Adds PCA-related command-line arguments to a given parser object.
     """
 
-    parser.add_argument("--systemf-text-pca", type=float, default=0.0)
-    parser.add_argument("--speechf-text-pca", type=float, default=0.0)
-    parser.add_argument("--speechf-wav-pca", type=float, default=0.0)
+    parser.add_argument("--systemf_text_pca", type=float, default=0.0)
+    parser.add_argument("--speechf_text_pca", type=float, default=0.0)
+    parser.add_argument("--speechf_wav_pca", type=float, default=0.0)
 
 def add_svm_args(parser: argparse.ArgumentParser):
     """
@@ -36,11 +36,11 @@ def add_lstm_args(parser: argparse.ArgumentParser):
     """
 
     parser.add_argument("--bidirectional", action="store_true")
-    parser.add_argument("--use-attention", action="store_true")
-    parser.add_argument("--hidden-size", type=int, default=128)
-    parser.add_argument("--num-layers", type=int, nargs='+', default=[1, 2, 3, 4])
+    parser.add_argument("--use_attention", action="store_true")
+    parser.add_argument("--hidden_size", type=int, default=128)
+    parser.add_argument("--num_layers", type=int, nargs='+', default=[1, 2, 3, 4])
     parser.add_argument("--lr", type=float, nargs='+', default=[0.001, 0.0005])
-    parser.add_argument("--batch-size", type=int, nargs='+', default=[5, 15, 25])
+    parser.add_argument("--batch_size", type=int, nargs='+', default=[5, 15, 25])
     parser.add_argument("--epochs", type=int, default=250)
 
     # @staticmethod
