03dbbaa2f5167367af79060bc3a41a44c7415b5f
Paul Gering <prgering1@sheffield.ac.uk>
2026-09-08 14:49:19 +0100

    debug shell script

---
+++ slurm/scripts/hyperparam_tune_system_lstm_debug.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=system_lstm_tuning
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=40G
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=4
+#
+# ---USER REQUIRED---
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --output=logs/system_lstm_tuning/%x_%A_%a.txt
+
+# --- Source Shared Environment Setup ---
+source slurm/scripts/setup_env.sh
+
+# Ensure output log folder exists
+mkdir -p logs/system_lstm_tuning
+
+# --- Execution ---
+python -m scripts.hyperparam_tuning_lstm \
+    system \
+    --use_attention \
+    --hidden_size 256 \
+    --systemf_text_pca 0.6 \
+    --pretrained_text_model sbert
