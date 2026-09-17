27b62e150765258b3bf40a6cbba90fc01ef4d37d
Paul Gering <prgering1@sheffield.ac.uk>
2026-09-17 13:52:38 +0100

    shell script to train best speech config

---
+++ slurm/scripts/hyperparam_tune_speech_lstm_debug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=speech_lstm_tuning
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=40G
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=4
+#
+# ---USER REQUIRED---
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --output=logs/speech_lstm_tuning/%x_%A_%a.txt
+
+# --- Source Shared Environment Setup ---
+source slurm/scripts/setup_env.sh
+
+# Ensure output log folder exists
+mkdir -p logs/speech_lstm_tuning
+
+# Execute your Python training script, redirecting stdout and stderr
+python -m scripts.hyperparam_tuning_lstm \
+    speech \
+    --use_attention \
+    --hidden_size 384 \
+    --speechf_text_pca 0.8 \
+    --speechf_speech_pca 0.6 \
+    --pretrained_text_model "roberta" \
+    --pretrained_speech_model "wavlm"
